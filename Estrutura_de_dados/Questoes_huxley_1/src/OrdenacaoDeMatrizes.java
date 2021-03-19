242b9151c571860bda6a2c06b9746b0a9fedec13
Eliseu Oliveira <eliseu.uepb@gmail.com>
2021-03-19 10:26:46 -0300

    huxleyy

---
+++ Estrutura_de_dados/Questoes_huxley_1/src/OrdenacaoDeMatrizes.java
@@ -0,0 +1,31 @@
+/**
+ * colher M e N 
+ *  numero de linhas menor ou igual a 10 M<=10
+ * numero de c
+ */
+
+public class OrdenacaoDeMatrizes {
+    
+}
+/**
+ * void ordenaMatriz (int n, int mat[n][n]) {
+        int i, j, l, c;
+
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                
+                for (l = 0; l < n; l++) {
+                    for (c = 0; c < n; c++) {
+                        
+                        if (mat[i][j] < mat[l][c]) {
+                            int aux = mat[i][j];
+                            mat[i][j] = mat[l][c];
+                            mat[l][c] = aux;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+ */
